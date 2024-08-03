6a9549973f9626a9f0191c44dcf05231b2b47c62
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-08-03 15:08:46 +0530

    add components to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
-
+import { Teams } from "@/components/Teams";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
+      <div className="flex justify-center items-center">
+        <Teams />
+      </div>
     </>
   );
 }
